230d95c7428e99447cd4077d1943ce2f3e0c9e7f
Ananpal Singh Sandhu <anansandhu@users.noreply.github.com>
2018-09-24 02:29:55 -0400

    first commit

---
+++ lab1/Controllers/ProductsController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace lab1.Controllers
+{
+    public class ProductsController : Controller
+    {
+        // GET: Products
+        public ActionResult Index()
+        {
+            return View();
+        }
+        // GET: Products/Product
+        public ActionResult Product( string ProductName)
+        {
+            ViewBag.ProductName = ProductName;
+            return View();
+        }
+    }
+}
